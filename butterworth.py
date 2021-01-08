88ab6f3ef7e9181e59f2940028ec06b59c02b7a2
DHasper <dylanhasperhoven@gmail.com>
2021-01-08 19:17:23 +0100

    Added .wav writing and SciPy butterworth bandpass filter

---
+++ butterworth.py
@@ -0,0 +1,126 @@
+from scipy.signal import lfilter
+
+import numpy
+import numpy as np
+from numpy import (atleast_1d, poly, polyval, roots, real, asarray,
+                   resize, pi, absolute, logspace, r_, sqrt, tan, log10,
+                   arctan, arcsinh, sin, exp, cosh, arccosh, ceil, conjugate,
+                   zeros, sinh, append, concatenate, prod, ones, full, array,
+                   mintypecode)
+
+def _relative_degree(z, p):
+    return len(p) - len(z)
+
+def lp2bp_zpk(z, p, k, wo=1.0, bw=1.0):
+    z = atleast_1d(z)
+    p = atleast_1d(p)
+    wo = float(wo)
+    bw = float(bw)
+
+    degree = _relative_degree(z, p)
+
+    # Scale poles and zeros to desired bandwidth
+    z_lp = z * bw/2
+    p_lp = p * bw/2
+
+    # Square root needs to produce complex result, not NaN
+    z_lp = z_lp.astype(complex)
+    p_lp = p_lp.astype(complex)
+
+    # Duplicate poles and zeros and shift from baseband to +wo and -wo
+    z_bp = concatenate((z_lp + sqrt(z_lp**2 - wo**2),
+                        z_lp - sqrt(z_lp**2 - wo**2)))
+    p_bp = concatenate((p_lp + sqrt(p_lp**2 - wo**2),
+                        p_lp - sqrt(p_lp**2 - wo**2)))
+
+    # Move degree zeros to origin, leaving degree zeros at infinity for BPF
+    z_bp = append(z_bp, zeros(degree))
+
+    # Cancel out gain change from frequency scaling
+    k_bp = k * bw**degree
+
+    return z_bp, p_bp, k_bp
+
+def bilinear_zpk(z, p, k, fs):
+    z = atleast_1d(z)
+    p = atleast_1d(p)
+
+    degree = _relative_degree(z, p)
+
+    fs2 = 2.0*fs
+
+    # Bilinear transform the poles and zeros
+    z_z = (fs2 + z) / (fs2 - z)
+    p_z = (fs2 + p) / (fs2 - p)
+
+    # Any zeros that were at infinity get moved to the Nyquist frequency
+    z_z = append(z_z, -ones(degree))
+
+    # Compensate for gain change
+    k_z = k * real(prod(fs2 - z) / prod(fs2 - p))
+
+    return z_z, p_z, k_z
+
+def zpk2tf(z, p, k):
+    z = atleast_1d(z)
+    k = atleast_1d(k)
+    if len(z.shape) > 1:
+        temp = poly(z[0])
+        b = np.empty((z.shape[0], z.shape[1] + 1), temp.dtype.char)
+        if len(k) == 1:
+            k = [k[0]] * z.shape[0]
+        for i in range(z.shape[0]):
+            b[i] = k[i] * poly(z[i])
+    else:
+        b = k * poly(z)
+    a = atleast_1d(poly(p))
+
+    # Use real output if possible. Copied from numpy.poly, since
+    # we can't depend on a specific version of numpy.
+    if issubclass(b.dtype.type, numpy.complexfloating):
+        # if complex roots are all complex conjugates, the roots are real.
+        roots = numpy.asarray(z, complex)
+        pos_roots = numpy.compress(roots.imag > 0, roots)
+        neg_roots = numpy.conjugate(numpy.compress(roots.imag < 0, roots))
+        if len(pos_roots) == len(neg_roots):
+            if numpy.all(numpy.sort_complex(neg_roots) == numpy.sort_complex(pos_roots)):
+                b = b.real.copy()
+
+    if issubclass(a.dtype.type, numpy.complexfloating):
+        # if complex roots are all complex conjugates, the roots are real.
+        roots = numpy.asarray(p, complex)
+        pos_roots = numpy.compress(roots.imag > 0, roots)
+        neg_roots = numpy.conjugate(numpy.compress(roots.imag < 0, roots))
+        if len(pos_roots) == len(neg_roots):
+            if numpy.all(numpy.sort_complex(neg_roots) == numpy.sort_complex(pos_roots)):
+                a = a.real.copy()
+
+    return b, a
+
+def buttap(N):
+    z = numpy.array([])
+    m = numpy.arange(-N+1, N, 2)
+    # Middle value is 0 to ensure an exactly real pole
+    p = -numpy.exp(1j * pi * m / (2 * N))
+    k = 1
+    return z, p, k
+
+def butter(order, Wn, btype='band'):
+    Wn = asarray(Wn)
+    z, p, k = buttap(order)
+
+    fs = 2.0
+    warped = 2 * fs * tan(pi * Wn / fs)
+
+    bw = warped[1] - warped[0]
+    wo = sqrt(warped[0] * warped[1])
+    z, p, k = lp2bp_zpk(z, p, k, wo=wo, bw=bw)
+    z, p, k = bilinear_zpk(z, p, k, fs=fs)
+    return zpk2tf(z, p, k)
+
+def butter_filter(data, samplerate, lowcut, highcut, order):
+    nyq = 0.5 * samplerate
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high])
+    return lfilter(b, a, data)
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import wave, struct
 import matplotlib.pyplot as plt
 import numpy as np
+from butterworth import butter_filter
+from datetime import datetime
+
+file = 'single bird'
+highcut = 5000
+lowcut = 0
 
 def readwav(file):
     wav = wave.open(file)
@@ -16,26 +22,36 @@ def readwav(file):
     # Convert data van binary naar integers
     integer_data = struct.unpack(fmt, binary_data)
 
-    return([framerate, nchannels, nframes, integer_data])
+    return([framerate, nchannels, nframes, integer_data]) 
 
 def plot_fourier(nframes, samplerate, data):
-
     # Neem een subset van de data
-    start=10000
-    duration=10000
+    start=64100
+    duration=1000
     stop=start+duration
-    data = data[start:stop]
+    # data = data[start:stop]
     intervals=len(data)
+
+    # Filter data
+    Ts = 1/samplerate
+    w_co=2*np.pi*1
+    a=np.array([1+w_co*Ts,-1])
+    b=np.array([w_co*Ts])
+    print(a, b)
+
+    data_filtered = butter_filter(data, samplerate, 1000, 4000, 5)
 
     # Fourier transformatie
     sp = abs(2/intervals*np.fft.fft(data))
+    sp_filtered = abs(2/intervals*np.fft.fft(data_filtered))
     freq = np.fft.fftfreq(intervals,1/samplerate)
 
     # Alleen de eerste helft van het data is bruikbaar
     freq=freq[0:int(len(data)/2)]
     sp=sp[0:int(len(data)/2)]
+    sp_filtered=sp_filtered[0:int(len(data_filtered)/2)]
 
-    plt.plot(freq, sp,'^r')
+    plt.plot(freq, sp,'r', freq, sp_filtered, 'b')
     # plt.yscale('log')
     plt.title('FFT')
     plt.xlabel('Frequency [Hz]')
@@ -43,7 +59,6 @@ def plot_fourier(nframes, samplerate, data):
     plt.show()
 
 def plot_data(nframes, samplerate, data):
-
     # Array voor de x as
     time = np.arange(0,nframes/samplerate,1/samplerate)
 
@@ -58,8 +73,17 @@ def plot_data(nframes, samplerate, data):
     plt.ylabel('Amplitude ')
     plt.show()
 
+def write_wav(samplerate, data):
+    with wave.open("output/%s-%s-%s.wav" % (file, lowcut, highcut), "w") as f:
+        f.setnchannels(2)
+        f.setsampwidth(2)
+        f.setframerate(samplerate)
+        for frame in data:
+            frame = struct.pack('<h', round(frame))
+            f.writeframesraw(frame)
+
 def main():
-    data = readwav('multiple birds.wav')
+    data = readwav(file + '.wav')
 
     samplerate = data[0]
     nchannels = data[1]
@@ -67,8 +91,17 @@ def main():
     data = data[3]
 
     # plot_data(nframes, samplerate, data)
-    plot_fourier(nframes, samplerate, data)
-
+    # plot_fourier(nframes, samplerate, data)
+    data_filtered = butter_filter(data, samplerate, lowcut, highcut, 5)
+    write_wav(samplerate, data_filtered)
+    # step = 200
+    # global lowcut, highcut
+    # for i in range(1000, 6000):
+    #     if i % step == 0:
+    #         lowcut = i
+    #         highcut = i + step
+    #         data_filtered = butter_filter(data, samplerate, lowcut, highcut, 5)
+    #         write_wav(samplerate, data_filtered)
 
 if __name__== '__main__':
     main()
